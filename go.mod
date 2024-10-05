b6f15b93d665fb80b6c9a94c508707d7da405c64
Ruslan Sotnikov <ruslan.sot70@gmail.com>
2024-10-05 09:43:31 +0300

    fix: migrate to chzyer/readline to fix bugs with sighup reload

--- go.mod
+++ go.mod
@@ -2,16 +2,19 @@ module uni_shell
 
 go 1.22.6
 
-require github.com/charmbracelet/lipgloss v0.13.0
+require (
+	github.com/charmbracelet/lipgloss v0.13.0
+	github.com/chzyer/readline v1.5.1
+	github.com/dustin/go-humanize v1.0.1
+	github.com/fiorix/go-readline v0.0.0-20180828120103-852e0b48d4eb
+	github.com/hajimehoshi/go-mp3 v0.3.4
+	github.com/hajimehoshi/oto/v2 v2.4.2
+)
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/x/ansi v0.1.4 // indirect
-	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/ebitengine/purego v0.4.1 // indirect
-	github.com/fiorix/go-readline v0.0.0-20180828120103-852e0b48d4eb // indirect
-	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
-	github.com/hajimehoshi/oto/v2 v2.4.2 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
--- internal/shell.go
+++ internal/shell.go
@@ -3,33 +3,57 @@ package internal
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/signal"
+	"os/user"
+	"path/filepath"
 	"strings"
 	"syscall"
 	"uni_shell/internal/commands"
 	"uni_shell/internal/commands/std"
-	"uni_shell/internal/history"
 
 	"github.com/charmbracelet/lipgloss"
-	"github.com/fiorix/go-readline"
+	"github.com/chzyer/readline"
 )
 
+// TODO: This cleanup function does not guarantee that readlinen will exist safely
 func cleanup(playSound bool) {
-	fmt.Println("running cleanup...")
 	if playSound {
 		std.PlayShutdownSound()
 	}
-	err := history.SaveHistoryFile()
-	if err != nil {
-		fmt.Println("Unable to write to history file: ", err)
+}
+
+func buildPrompt(user *user.User, prevResult *commands.CmdResult, highlightStyle lipgloss.Style, errStyle lipgloss.Style) string {
+	stdPrompt := highlightStyle.Render("» ")
+
+	wd, _ := os.Getwd()
+	wd = strings.Replace(wd, user.HomeDir, "~", 1)
+
+	finalPrompt := fmt.Sprintf("(%v) %v", wd, stdPrompt)
+	if prevResult != nil && prevResult.ExitCode != 0 {
+		finalPrompt = errStyle.Render(fmt.Sprintf("[%d] ", prevResult.ExitCode)) + finalPrompt
 	}
+
+	return finalPrompt
+}
+
+func handleConfigReload(l *readline.Instance) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGHUP)
+	go func() {
+		for s := range sigs {
+			if s == syscall.SIGHUP {
+				fmt.Println("\nrush configuration reloaded...")
+				l.Refresh()
+				return
+			}
+		}
+	}()
 }
 
 func RunShell() {
-	// play sound flag parsing
-	playSound := flag.Bool("silent", false, "Play startup/shutdown sound")
-
 	highlightStyle := lipgloss.NewStyle().
 		Bold(true).
 		Foreground(lipgloss.Color("#7D56F4"))
@@ -37,87 +61,100 @@ func RunShell() {
 		Bold(true).
 		Foreground(lipgloss.Color("9"))
 
-	history.InitHistory()
-	sighup := false
-	stdPrompt := highlightStyle.Render("rush> ")
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:            "\033[31m\033[0m ",
+		HistoryFile:       filepath.Join(usr.HomeDir, ".rush_history"),
+		InterruptPrompt:   "^C",
+		EOFPrompt:         ":q",
+		HistorySearchFold: true,
+		VimMode:           true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	l.CaptureExitSignal()
 
-	sigs := make(chan os.Signal, 5)
-	signal.Notify(sigs, os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGKILL)
-	go func() {
-		for s := range sigs {
-			if s == syscall.SIGHUP {
-				sighup = true
-				go std.PlayBootSound()
-				return
-			}
+	setPasswordCfg := l.GenPasswordConfig()
+	setPasswordCfg.SetListener(func(line []rune, pos int, key rune) (newLine []rune, newPos int, ok bool) {
+		l.SetPrompt(fmt.Sprintf("Enter password(%v): ", len(line)))
+		l.Refresh()
+		return nil, 0, false
+	})
 
-			cleanup(*playSound)
+	log.SetOutput(l.Stderr())
 
-			// interrupt signal received
-			os.Exit(0)
-		}
-	}()
+	go handleConfigReload(l)
 
-	if *playSound {
+	// play sound flag parsing
+	bootSound := flag.Bool("silent", false, "Play startup/shutdown sound")
+	if *bootSound {
 		go std.PlayBootSound()
 	}
 
 	var prevResult *commands.CmdResult = nil
 	for {
-		wd, _ := os.Getwd()
-		wd = strings.Replace(wd, os.Getenv("HOME"), "~", 1)
-
-		finalPrompt := fmt.Sprintf("(%v) %v", wd, stdPrompt)
-		if prevResult != nil && prevResult.ExitCode != 0 {
-			finalPrompt = errStyle.Render(fmt.Sprintf("[%d] ", prevResult.ExitCode)) + finalPrompt
-		}
-		input := readline.Readline(&finalPrompt)
-
-		switch {
-		case input == nil:
-			if !sighup {
-				cleanup(*playSound)
-				os.Exit(0)
-			}
-		case *input != "": // Ignore blank lines
-			input := *input
-			input = strings.TrimSpace(input)
-			if input == "" {
+		l.SetPrompt(buildPrompt(
+			usr,
+			prevResult,
+			highlightStyle,
+			errStyle,
+		))
+		line, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
 				continue
 			}
-			history.SaveCmdToHistory(input)
-			cmd := commands.ParseCommandFromString(input)
-
-			var result *commands.CmdResult = nil
-			if !cmd.IsStd {
-				result = commands.RunFromPath(cmd)
-			} else {
-				result = std.RunStdCmd(cmd)
-			}
-
-			if result == nil {
-				result = &commands.CmdResult{
-					ExitCode: 127,
-					Output:   fmt.Sprintf("No command found: %v", cmd.Name),
-				}
-			}
-
-			output := result.Output
-			if result.Output != "" {
-				fmt.Println(output)
-			}
-
-			if result.Flags.Exit {
-				cleanup(*playSound)
-				os.Exit(result.ExitCode)
-			}
-
-			prevResult = result
+		} else if err == io.EOF {
+			break
 		}
 
-		if sighup {
-			sighup = false
-			fmt.Println(highlightStyle.Render("rush configuration reloaded..."))
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
 		}
+
+		// I know that this is a hack, but I don't wanna refactor so that comamnds have access to the readline instance
+		if line == ":exitvim" {
+			l.SetVimMode(false)
+			continue
+		}
+		if line == ":vim" {
+			l.SetVimMode(true)
+			continue
+		}
+
+		cmd := commands.ParseCommandFromString(line)
+		var result *commands.CmdResult = nil
+		if !cmd.IsStd {
+			result = commands.RunFromPath(cmd)
+		} else {
+			result = std.RunStdCmd(cmd)
+		}
+
+		if result == nil {
+			result = &commands.CmdResult{
+				ExitCode: 127,
+				Output:   fmt.Sprintf("No command found: %v", cmd.Name),
+			}
+		}
+
+		output := result.Output
+		if result.Output != "" {
+			fmt.Println(output)
+		}
+
+		if result.Flags.Exit {
+			cleanup(*bootSound)
+			os.Exit(result.ExitCode)
+		}
+
+		prevResult = result
 	}
 }
